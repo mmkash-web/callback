44f85a496a9845eac64b0b27890976933185662c
mmkash-web <manuuduke0@gmail.com>
2024-10-11 18:02:44 +0300

    Update callback.py

--- callback.py
+++ callback.py
@@ -59,3 +59,4 @@ def get_user_id_from_transaction(transaction_reference):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable provided by Heroku
     app.run(host='0.0.0.0', port=port)  # Bind to all interfaces
+
